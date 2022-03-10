b86b4e539258376c2efec51736e80ab23677a5e0
Kyle Chine <kyle.chine@leismore.co>
2022-03-11 10:47:41 +1100

    working now

--- fetch.js
+++ fetch.js
@@ -1,23 +1,28 @@
-const fs     = require('fs');
 const axios  = require('axios').default;
 const config = require('./config.json');
+const mysql  = require('promise-mysql');
+const SQL = 'INSERT price VALUES(null, ?, CURRENT_TIMESTAMP, ?, ?, ?);';
+
+
 
 module.exports = () => {
 
     axios.get(config.api)
     .then( resp => {
         console.log(resp.data);
-        config.coinList.forEach(code => {
-            let fileName = __dirname + '/price_' + code + '.csv';
-            let now = new Date();
-            let text = (
-                String(now) + ',' +
-                String(resp.data.prices[code].bid) + ',' +
-                String(resp.data.prices[code].ask) + ',' +
-                String(resp.data.prices[code].last) + '\n'
-            );
-            fs.appendFileSync(fileName, text);
-        })
+        return resp;
+    })
+    .then(resp => {
+        mysql.createConnection({
+            user: config.mysql.user,
+            password: config.mysql.password,
+            host: config.mysql.host,
+            database: config.mysql.database
+        }).then(db=>{
+            config.coinList.forEach(code => {
+                db.query(SQL,[code, resp.data.prices[code].bid, resp.data.prices[code].ask, resp.data.prices[code].last]);
+            })
+        });
     })
     .catch(e=>{
         process.exitCode = 5;
